6229bc7426e0d0b0820bdafaa18fc075a276c14f
Shauviq <shauviqmishra@gmail.com>
2025-04-12 13:09:02 +0530

    initial Setup

---
+++ frontend/src/Components/Pages/SignupPage.jsx
@@ -0,0 +1,14 @@
+import Header from "../Header/Header";
+import Signup from "../Signup/Signup";
+
+function SignupPage(){
+    return(
+        <div>
+            <Header/>
+            <Signup/>
+        </div>
+    )
+}
+
+
+export default SignupPage;
